2d07c08246a8be9a3536c56c641baa7bac090021
templar-ajay <templar.ajay@gmail.com>
2023-11-28 15:30:34 +0530

    add popup on cta

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,7 @@
 import clsx from "clsx";
 import { PrismicNextLink, PrismicNextLinkProps } from "@prismicio/next";
 import { createClient } from "@/prismicio";
+import Popup from "@/components/Popup";
 
 export default async function Button({
   className,
@@ -9,16 +10,19 @@ export default async function Button({
   const settings = await createClient().getSingle("settings");
   const { cta_background_color, cta_text_color } = settings.data;
   return (
-    <PrismicNextLink
-      className={clsx(
-        "block w-full bg-yellow-400 hover:bg-yellow-500 transition-colors duration-200 ease-in-out py-4 md:py-6 px-8 md:px-12 font-display font-semibold text-lg md:text-2xl text-center text-white tracking-wide",
-        className
-      )}
-      style={{
-        color: cta_text_color || "rgb(250,204,21)",
-        backgroundColor: cta_background_color || "rgb(234,179,8)",
-      }}
-      {...restProps}
-    />
+    <div className="relative">
+      <Popup />
+      <PrismicNextLink
+        className={clsx(
+          "block w-full bg-yellow-400 hover:bg-yellow-500 transition-colors duration-200 ease-in-out py-4 md:py-6 px-8 md:px-12 font-display font-semibold text-lg md:text-2xl text-center text-white tracking-wide",
+          className
+        )}
+        style={{
+          color: cta_text_color || "rgb(250,204,21)",
+          backgroundColor: cta_background_color || "rgb(234,179,8)",
+        }}
+        {...restProps}
+      />
+    </div>
   );
 }
